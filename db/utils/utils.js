e72e01b21315ccafc98a2d8038a1078c2fe96006
Colum John Lynagh <columjohnlynagh@gmail.com>
2019-06-24 14:52:32 +0100

    created function for creating reference object and starting function for formatting comments

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -6,6 +6,22 @@ exports.formatDate = list => {
   });
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  return list.reduce((acc, cur) => {
+    acc[cur.title] = cur.article_id;
+    return acc;
+  }, {});
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  return comments.map(commentsCheck => {
+    if (!articleRef) {
+      return commentsCheck;
+    } else {
+      const key = commentsCheck.created_by;
+      commentsCheck.author = articleRef[key];
+      delete commentsCheck.created_by;
+      return commentsCheck;
+    }
+  });
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -82,6 +82,50 @@ describe('formatDate', () => {
   });
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', list => {
+  it('returns an empty object when invoked with an empty array', () => {
+    const list = [];
+    const actual = makeRefObj(list);
+    const expected = {};
+    expect(actual).to.eql(expected);
+  });
+  it('takes an array with a single object and returns a reference object keys with each items title and the values being each items corresponding id', () => {
+    const list = [{ article_id: 1, title: 'A' }];
+    const actual = makeRefObj(list);
+    const expected = { A: 1 };
+    expect(actual).to.eql(expected);
+  });
+  it('takes an array of multiple objects and returns a reference object keys with each items title and the values being each items corresponding id', () => {
+    const list = [{ article_id: 1, title: 'A' }, { article_id: 2, title: 'B' }];
+    const actual = makeRefObj(list);
+    const expected = { A: 1, B: 2 };
+    expect(actual).to.eql(expected);
+  });
+});
 
-describe('formatComments', () => {});
+// This utility function should be able to take an array of comment objects (comments) and a reference object, and return a new array of formatted comments.
+
+// Each formatted comment must have:
+
+// Its created_by property renamed to an author key
+// Its belongs_to property renamed to an article_id key
+// The value of the new article_id key must be the id corresponding to the original title value provided
+// Its created_at value converted into a javascript date object
+// The rest of the comment's properties must be maintained
+
+describe('formatComments', (comments, articleRef) => {
+  it('returns a new empty array, when passed an empty array', () => {
+    const comments = [];
+    const articleRef = {};
+    const actual = formatComments(comments, articleRef);
+    const expected = [];
+    expect(actual).to.eql(expected);
+  });
+  it('takes an array with an object containing a created_by property and returns a new array with this property renamed to an author key', () => {
+    const comments = [{ created_by: 'tickle122' }];
+    const articleRef = { author: 'tickle122' };
+    const actual = formatComments(comments, articleRef);
+    const expected = [{ author: 'tickle122' }];
+    expect(actual).to.eql(expected);
+  });
+});
