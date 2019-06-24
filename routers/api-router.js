4e2c6bd8f094a5fd611192771f458ea85c9b4db5
Colum John Lynagh <columjohnlynagh@gmail.com>
2019-06-24 19:37:45 +0100

    added basic tests along with controller and model for GET /api/users/:username endpoint - need to add some error handling before moving on

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,12 +1,12 @@
 const apiRouter = require('express').Router();
 const topicsRouter = require('./topics-router');
 // const articlesRouter = require('./articles-router');
-// const usersRouter = require('./users-router');
+const usersRouter = require('./users-router');
 // const commentsRouter = require('./comments-router');
 
 apiRouter.use('/topics', topicsRouter);
 
-// apiRouter.use('/users', usersRouter);
+apiRouter.use('/users', usersRouter);
 
 // apiRouter.use('/articles', articlesRouter);
 
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -21,4 +21,24 @@ describe('/api', () => {
         });
     });
   });
+  describe('/users/:username', () => {
+    it('GET status: 200 and returns an object of the user passed in the url along with the correct properties present', () => {
+      return request(app)
+        .get(`/api/users/tickle122`)
+        .expect(200)
+        .then(res => {
+          // console.log(res.body.user, '<--');
+          expect(res.body.user.username).to.equal('tickle122');
+          expect(res.body.user).to.contain.keys('username', 'avatar_url', 'name');
+        });
+    });
+    // it('GET: 400 bad request when requesting an invalid user', () => {
+    //   return request(app)
+    //     .get('/api/users/rjifberie123')
+    //     .expect(400)
+    //     .then(res => {
+    // //       expect(res.body.msg).to.equal('bad request');
+    //     });
+    // });
+  });
 });
